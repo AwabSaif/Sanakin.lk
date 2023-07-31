32d701f6131b602e8999da23866dc484670dab89
AwabSaif <awabtaz9090@gmail.com>
2023-07-31 23:40:29 +0300

    add hero about us

--- src/Page/AboutUs/AboutUs.jsx
+++ src/Page/AboutUs/AboutUs.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './AboutUs.css'
+import HeroAbuotus from '../../Sections/AboutUs/HeroAboutus/HeroAboutus'
 
 export default function AboutUs() {
   return (
-    <div>AboutUs</div>
+    <div className='aboutus-container'>
+        <HeroAbuotus />
+    </div>
   )
 }
---
+++ src/Sections/AboutUs/HeroAboutus/HeroAboutus.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './HeroAboutus.css'
+
+export default function HeroAbuotus() {
+  return (
+    <div className='heroabuotus' >
+      <h2>About Us</h2>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+      </div>
+  )
+}
